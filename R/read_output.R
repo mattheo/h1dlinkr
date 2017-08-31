a0761bc7600a356aea2bdcdc4201a446686c74ba
mattheo <mattheo@users.noreply.github.com>
2017-08-31 21:22:15 +0200

    Changes to output parse functions. Start working on read_balance

--- R/read_output.R
+++ R/read_output.R
@@ -1,16 +1,29 @@
-#' Read from HYDRUS-1D output files
-#'
-#' @import tibble magrittr stringr
-#' @name read_output
-NULL
-
+#' @import tibble magrittr stringr readr
 extr_col_names <- function(string) {
   string %>%
-    str_trim() %>%
-    str_split("\\s+", simplify = TRUE) %>%
+    str_trim() %>% # trim whitespace from start and end
+    str_split("\\s+", simplify = TRUE) %>% # split the string at whitespace
     # str_to_lower() %>%
-    str_replace("\\(|\\/", "_") %>%
-    str_replace("\\)", "")
+    str_replace("\\(|\\/", "_") %>% #replace opening parentheses and slashes with lodash
+    str_replace("\\)", "") # remove closing parentheses
+}
+
+#' Read HYDRUS-1D output files into R
+#'
+#' @param path
+#'
+#' @return list of data frames
+#' @export
+#' @name read_output
+read_output <- function(path = ".") {
+  files <- list.files(path, full.names = T)
+  structure(list(
+    t_level = read_t_level(str_subset(files, "(?i)t_Level")),
+    a_level = read_a_level(str_subset(files, "(?i)a_Level")),
+    nod_inf = read_nod_inf(str_subset(files, "(?i)nod_inf")),
+    obs_node = read_obs_node(str_subset(files, "(?i)obs_node"))
+    # stdout = read_stdout(str_subset(files, "(?i)run.out"))
+  ), class = "h1d_output")
 }
 
 #' @param file_path
@@ -21,49 +34,60 @@ extr_col_names <- function(string) {
 #' @export
 #' @rdname read_output
 read_t_level <- function(file_path, skip = 6) {
-  t_level_lines <- readr::read_lines(file_path, skip)
+  t_level_lines <- read_lines(file_path, skip)
 
-  readr::read_table(
+  out <- read_table(
     file = file_path,
     col_names = extr_col_names(t_level_lines[1]),
-    skip = skip + 3, # The first 3 lines are the header
-    col_types = readr::cols(.default = "d"),
+    skip = skip + 3, # The first 3 lines are header
+    col_types = cols(.default = "d"),
     n_max = sum(str_count(t_level_lines, "^ +\\d+"))
   )
+  attributes(out) <- attributes(out)[c("names", "row.names", "class")]
+  return(out)
+}
+
+
+#' @return data.frame
+#' @export
+#'
+#' @rdname read_output
+read_a_level <- function(file_path, skip = 2) {
+  read_t_level(file_path, skip)
 }
 
 #' @return list of data.frames
 #' @export
 #' @rdname read_output
 read_nod_inf <- function(file_path) {
-  nod_inf_lines <- readr::read_lines(file_path)
-  col_names <- extr_col_names(nod_inf_lines[11])[-1] # first column unnecessary
+  nod_inf_lines <- read_lines(file_path)
 
-  pattern <- "^[ \\t]*Time:\\s+"
+  col_names <- extr_col_names(nod_inf_lines[11])
+  time_idx <- str_which(nod_inf_lines, "^[ \\t]*Time:\\s+")
   time_stamp <-
-    str_subset(nod_inf_lines, pattern) %>%
+    nod_inf_lines[time_idx] %>%
     str_extract("[0-9]+\\.?[0-9]*") %>%
     as.double()
+  slice_start <- time_idx + 6
+  len <- (str_which(nod_inf_lines, "^end")[1] - slice_start[1])
+  idx <- sapply(slice_start, seq.int, length.out = len)
+  depth <-
+    read_table(
+      file_path,
+      col_names = col_names,
+      col_types = cols_only(Depth = "d"),
+      skip = slice_start[1] - 1,
+      n_max = len
+    ) %>%
+    unlist(use.names = FALSE)
 
-  skip <- str_which(nod_inf_lines, pattern) + 5
-  n_max <- (str_which(nod_inf_lines, "^end") - skip - 1)[1]
-
-  blocks <- array(dim = c(n_max, length(col_names), length(time_stamp)),
-                  dimnames = list(seq_len(n_max), col_names, time_stamp))
-  for (i in seq_along(time_stamp)) {
-    blocks[, , i] <-
-      readr::read_table(
-        file = file_path,
-        col_types = readr::cols(.default = "d"),
-        col_names = FALSE,
-        skip = skip[i],
-        n_max = n_max
-      )[, -1] %>%
-      as.matrix()
-    # colnames(blocks[, , i]) <-
-  }
-  # dimnames(blocks) <-
-  aperm(blocks, c(3, 1, 2))
+  read_table(
+    file = paste0(nod_inf_lines[idx], collapse = "\n"),
+    col_types = cols(.default = "d"),
+    col_names = col_names
+  )[, -(1:2)] %>% # remove node and depth columns
+    lapply(matrix, nrow = len) %>%
+    structure(x = time_stamp, z = depth)
 }
 
 #' @param header
@@ -73,37 +97,66 @@ read_nod_inf <- function(file_path) {
 #' @export
 #' @rdname read_output
 read_obs_node <- function(file_path, header = 11) {
-  obs_node_header <- readr::read_lines(file_path, n_max = header)
+  obs_node_header <- read_lines(file_path, n_max = header)
   col_names <-
     str_subset(obs_node_header, "^[ ]*time") %>%
     extr_col_names() %>%
     unique()
+  if (length(col_names) == 0) return(NULL)
 
   nodes <-
     str_subset(obs_node_header, pattern = "Node") %>%
     str_extract_all(pattern = "[0-9]+", simplify = TRUE) %>%
     as.integer()
 
-  par_per_node <- length(col_names)
+  par_per_node <- length(col_names) - 1
 
   data <-
-    readr::read_table(
+    read_table(
       file = file_path,
       skip = header,
       col_names = FALSE,
-      col_types = readr::cols(.default = "d")
+      col_types = cols(.default = "d")
     )
 
-  for(i in seq_along(nodes))
-  node <- data[1:par_per_node]
-  colnames(node) <- col_names
-  node["node"] <- nodes[1]
+  out <- lapply(seq_along(nodes), function(i) {
+    node <- data[c(1, ((i - 1) * par_per_node + 2):(i * par_per_node + 1))]
+    colnames(node) <- col_names
+    node["node"] <- nodes[i]
+    return(node)
+  }) %>%
+    {do.call("rbind", .)}
+}
+
+read_balance <- function(file_path) {
+  balance_lines <- read_lines(file_path)
+  time_idx <- str_which(balance_lines, "^[ \\t]*Time\\s+")
+  slice_idx <- sapply(time_idx, `+`, 4:9) %>% as.numeric()
+  time_stamp <-
+    balance_lines[time_idx] %>%
+    str_extract("[0-9]+\\.?[0-9]*") %>%
+    as.double()
+  col_names <-
+    balance_lines[time_idx[1] + 4:9] %>%
+    str_sub(end = 19) %>%
+    str_trim()
+  values <- str_sub(balance_lines[slice_idx], start = 20) %>% paste0(collapse = "\n")
+  values_df <-
+    read_table(
+      values,
+      col_types = cols(.default = "d"),
+      col_names = FALSE
+    ) %>%
+    unlist(use.names = FALSE) %>%
+    matrix(ncol = length(col_names), dimnames = list(NULL, col_names), byrow = TRUE)
+
+
 }
 
 #' @return data.frame
 #' @export
 #' @rdname read_output
 read_stdout <- function(file_path) {
-  stdout_lines <- readr::read_lines(file_path)
-  str_which(" Beginning of numerical solution.", stdout_lines[1:50])
+  stdout_lines <- read_lines(file_path)
+  # str_which(" Beginning of numerical solution.", stdout_lines[1:50])
 }
--- tests/testthat/test_output_parser.R
+++ tests/testthat/test_output_parser.R
@@ -2,8 +2,19 @@ library(h1dlinkr)
 
 context("Parse HYDRUS output")
 
-path <- "./tests/testthat/data/ga_l3_fit_100ET3L/"
+path <- file.path("./data/ga_l3_fit_100ET3L/")
+list.files(path, pattern = "\\.out")
 
-actual_t_level <- read_t_level(file.path(path, "T_level.out"))
-actual_nod_inf <- read_nod_inf(file.path(path, "Nod_Inf.out"))
-actual_obs_node <- read_obs_node(file.path(path, "Obs_Node.out"))
+t_level <- read_t_level(file.path(path, "T_level.out"))
+a_level <- read_a_level(file.path(path, "A_level.out"))
+nod_inf <- read_nod_inf(file.path(path, "Nod_Inf.out"))
+# image(y = rev(attr(nod_inf, "depth")), z = apply(exp(log(nod_inf[["Head"]])), 1, rev), col = cm.colors(10))
+# image(y = rev(attr(nod_inf, "depth")), z = apply((nod_inf[["Head"]]), 1, rev), col = (heat.colors(1000)), add = T)
+# image(x = attr(nod_inf, "time"), y = rev(attr(nod_inf, "depth")), z = apply(nod_inf[["Flux"]], 1, rev), col = topo.colors(1000))
+obs_node <- read_obs_node(file.path(path, "Obs_Node.out"))
+balance <- read_balance(file.path(path, "Balance.out"))
+run_stdout <- read_stdout(file.path(path, "run.out"))
+
+microbenchmark::microbenchmark(
+  output <- read_output(path)
+)
