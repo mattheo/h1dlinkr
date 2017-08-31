a955904516beaf5b4a7fd9d550b772580b3610e3
mattheo <mattheo@users.noreply.github.com>
2017-08-31 21:26:03 +0200

    change run_h1d so it can be run asynchronously

--- R/run_hydrus.R
+++ R/run_hydrus.R
@@ -10,33 +10,21 @@
 #'
 run_h1d <- function(project, h1d_exec = "h1d_calc", async = FALSE) {
   project <- file.path(project)
-  if (async) {
-    out <- file.path(project, "run.out")
-    if (file.exists(out)) unlink(out, force = TRUE)
-    return(processx::process$new(h1d_exec, project, stdout = out, stderr = out))
-  } else {
-    runtime <- system.time(
-      p <- processx::run(h1d_exec, project, timeout = 30)
-    )
-  }
-  structure(
-    read_output(project),
-    class = "h1d_output",
-    path = project,
-    success = stringr::str_detect(p$stdout, "Calculations have finished successfully."),
-    runtime = as.double(runtime["elapsed"]),
-    pid = Sys.getpid(),
-    process = p
-  )
-}
-
-#' @return processx object
-#' @export
-#' @rdname run_h1d
-run_h1d_async <- function(project, h1d_exec = "h1d_calc") {
-  project <- file.path(project)
   out <- file.path(project, "run.out")
   if (file.exists(out)) unlink(out, force = TRUE)
-  processx::process$new(h1d_exec, project, stdout = out, stderr = out)
-
+  if (async) {
+    return(processx::process$new(h1d_exec, project, stdout = out, stderr = out))
+  }
+  runtime <- system.time(
+    system2(h1d_exec, project, stdout = out, stderr = out)
+  )
+  run_stdout <- read_file(out)
+  structure(
+    read_output(project),
+    path = project,
+    success = str_detect(run_stdout, "Calculations have finished successfully."),
+    runtime = as.double(runtime["elapsed"]),
+    pid = Sys.getpid(),
+    stdout = run_stdout
+  )
 }
--- tests/testthat/test_run_h1d.R
+++ tests/testthat/test_run_h1d.R
@@ -1,2 +1,30 @@
 
-run_h1d("C:/Program Files (x86)/PC-Progress/Hydrus-1D 4.xx/H1D_CALC.EXE", "./tests/testthat/data/ga_l3_fit_100ET3L")
+project <- "./data/ga_l3_fit_100ET3L/"
+system.time(out <- run_h1d(project))
+print(attr(out, "runtime"))
+
+library(future)
+plan(multiprocess)
+system.time({
+  f <- future(run_h1d(project))
+  repeat {
+    if (resolved(f)) {
+      out <- value(f)
+      break
+    }
+  }
+})
+print(attr(out, "runtime"))
+
+system.time({
+  run <- run_h1d_async(project)
+  repeat {
+    Sys.sleep(0.001)
+    if (run$is_alive() == FALSE) {
+      break
+    }
+  }
+})
+
+
+
