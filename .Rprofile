2f757ca2e432351273e9c016b3d9c103644aada6
mattheo <mattheo@users.noreply.github.com>
2017-08-31 21:15:50 +0200

    update description, packrat, gitignore

--- .Rprofile
+++ .Rprofile
@@ -1,4 +1,4 @@
 cat("h1dlinkr directory")
-#### -- Packrat Autoloader (version 0.4.8-20) -- ####
+#### -- Packrat Autoloader (version 0.4.8-30) -- ####
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
--- R/run_hydrus.R
+++ R/run_hydrus.R
@@ -1,27 +1,42 @@
 #' Execute H1D from within R
 #'
+#' @param project
+#' Path to HYDRUS project folder
 #' @param h1d_exec
 #' Path to H1D_CALC executable
-#' @param project_path
-#' Path to HYDRUS project folder
 #' @import utils
 #' @return list
 #' @export
 #'
-run_h1d <- function(h1d_exec, project_path) {
-  outfile <- file.path(project_path, "run.out")
-  file.create(outfile)
-  runtime <- system.time(
-    errcode <- system2(h1d_exec, shQuote(project_path), stdout = outfile)
+run_h1d <- function(project, h1d_exec = "h1d_calc", async = FALSE) {
+  project <- file.path(project)
+  if (async) {
+    out <- file.path(project, "run.out")
+    if (file.exists(out)) unlink(out, force = TRUE)
+    return(processx::process$new(h1d_exec, project, stdout = out, stderr = out))
+  } else {
+    runtime <- system.time(
+      p <- processx::run(h1d_exec, project, timeout = 30)
+    )
+  }
+  structure(
+    read_output(project),
+    class = "h1d_output",
+    path = project,
+    success = stringr::str_detect(p$stdout, "Calculations have finished successfully."),
+    runtime = as.double(runtime["elapsed"]),
+    pid = Sys.getpid(),
+    process = p
   )
+}
 
-  list(
-    errcode = errcode,
-    path = project_path,
-    success = grepl("Calculations have finished successfully.",
-                    tail(readr::read_lines(outfile), 1)),
-    runtime = runtime["elapsed"],
-    pid = Sys.getpid()
-    # output = read_stdout(outfile)
-  )
+#' @return processx object
+#' @export
+#' @rdname run_h1d
+run_h1d_async <- function(project, h1d_exec = "h1d_calc") {
+  project <- file.path(project)
+  out <- file.path(project, "run.out")
+  if (file.exists(out)) unlink(out, force = TRUE)
+  processx::process$new(h1d_exec, project, stdout = out, stderr = out)
+
 }
