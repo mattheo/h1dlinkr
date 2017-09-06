5778f9e38d09ffc52717bab991eacee596583e21
mattheo <mattheo@users.noreply.github.com>
2017-09-06 13:20:44 +0200

    minor changes to multiprocess tests

--- tests/testthat/test_multiprocess.R
+++ tests/testthat/test_multiprocess.R
@@ -1,82 +1,109 @@
 library(future)
 library(doParallel)
 
-run_par <- function(runs, template, threads) {
-  plan(multiprocess, workers = threads)
-  # threads <- length(cl)
+run_future <- function(runs, options, folder, conjunct = FALSE) {
   fs <- list()
   for(j in seq_len(runs)) {
-    cat("create future", j, "\n")
-    # print(system.time(
-    fs[[j]] <- future(
-      {
-        current <- file.path("./tests/testthat/data/mp", formatC(j, width = 3, flag = 0))
-        unlink(current, recursive = TRUE, force = TRUE)
-        dir.create(current, recursive = TRUE)
-        file.copy(template, current)
-        h1dlinkr::run_h1d(current)
-        # Sys.sleep(5)
-        # Sys.getpid()
-      }
-    )
-    # ))
-    # the first few workers can be started without checking if they are finished
-    if(j < threads) next
+    cat(paste("\nStarting future", j), "\n")
+    fs[[j]] <-
+      future(
+        {
+          # create new project folder
+          path <- file.path(folder, sprintf("h1d_run_%05.f", id))
+          # write HYDRUS input files to the directory
+          h1dlinkr::write_input(path, options)
+          # run HYDRUS
+          run <- h1dlinkr::run_h1d(path)
+          # Sys.sleep(stats::rnorm(1, mean = 5, sd = 1))
+          # read output of run
+          h1dlinkr::read_output(run)
+        },
+        globals = list(id = j,
+                       options = options,
+                       folder = folder)
+      )
+    if(conjunct | j < nbrOfWorkers()) {
+      next
+    }
     repeat {
-      # cat(system.time({x <- resolved(fs)})["elapsed"])
-      cat(".")
-      if (sum(!resolved(fs)) < threads) {
-        cat("\nnext future\n")
+      print(system.time(
+        x <- resolved(fs)
+      ))
+      if (sum(!x) < nbrOfWorkers()) {
+        cat(paste(sum(!x), "unresolved futures"), "\n")
         break
       }
-      cat("-")
     }
   }
-  values(fs)
+  fs
 }
 
-run_serial <- function(runs, template) {
-  out <- list()
-  for(j in seq_len(runs)) {
-    current <- file.path("./tests/testthat/data/mp", formatC(j, width = 3, flag = 0))
-    unlink(current, recursive = TRUE, force = TRUE)
-    dir.create(current, recursive = TRUE)
-    file.copy(template, current)
-    out[[j]] <- h1dlinkr::run_h1d(current)
-  }
-  unlink("./tests/testthat/data/mp", recursive = T, force = T)
-  return(out)
-}
-
-run_foreach <- function(runs, template) {
-  out <- foreach(j = icount(runs)) %dopar% {
-    current <- file.path("./tests/testthat/data/mp", formatC(j, width = 3, flag = 0))
-    unlink(current, recursive = TRUE, force = TRUE)
-    dir.create(current, recursive = TRUE)
-    file.copy(template, current)
-    h1dlinkr::run_h1d(current)
-  }
-  unlink("./tests/testthat/data/mp", recursive = T, force = T)
-  return(out)
-}
-
-proj_template <- dir("./tests/testthat/data/ga_l3_fit_100ET3L/", full.names = TRUE)
-# serial <- system.time(a <- run_serial(10, proj_template))
-
-parl <- system.time(b <- run_par(20, proj_template, threads = 4))
-
-cl <- parallel::makeCluster(4)
-registerDoParallel(cl)
-doParallel <- system.time(c <- run_foreach(20, proj_template))
-parallel::stopCluster(cl)
+# h1d_future <- function(id, options, folder, ...) {
+#   future(
+#     {
+#       # create new project folder
+#       path <- file.path(folder, sprintf("h1d_run_%05.f", id))
+#       # write HYDRUS input files to the directory
+#       h1dlinkr::write_input(path, options)
+#       # run HYDRUS
+#       run <- h1dlinkr::run_h1d(path)
+#       # Sys.sleep(stats::rnorm(1, mean = 5, sd = 1))
+#       # read output
+#       h1dlinkr::read_output(run)
+#     },
+#     globals = c("id", "options", "folder"),
+#     ...
+#   )
+# }
 #
-# print(serial)
-# print(sapply(a, attr, which = "runtime"))
-print(parl)
+# run_serial <- function(runs, template) {
+#   out <- list()
+#   for(j in seq_len(runs)) {
+#     current <- file.path("./data/mp", formatC(j, width = 3, flag = 0))
+#     unlink(current, recursive = TRUE, force = TRUE)
+#     dir.create(current, recursive = TRUE)
+#     file.copy(template, current)
+#     out[[j]] <- h1dlinkr::run_h1d(current)
+#   }
+#   unlink("./data/mp", recursive = T, force = T)
+#   return(out)
+# }
+
+run_foreach <- function(runs, options, folder) {
+  foreach(j = icount(runs)) %dopar% {
+    # create new project folder
+    path <- file.path(folder, sprintf("h1d_run_%05.f", id))
+    # write HYDRUS input files to the directory
+    h1dlinkr::write_input(path, options)
+    # run HYDRUS
+    run <- h1dlinkr::run_h1d(path)
+    # Sys.sleep(stats::rnorm(1, mean = 5, sd = 1))
+    # read output of run
+    h1dlinkr::read_output(run)
+  }
+}
+
+tmpfolder <- function(length = 20, pattern = "[a-zA-Z0-9]") {
+  file.path(Sys.getenv("TMP"), stringi::stri_rand_strings(1, length = length, pattern))
+}
+
+
+cl <- parallel::makeForkCluster()
+project <- "~/Studium/Master Thesis/h1dlinkr/data/SA_test"
+
+plan(cluster, workers = cl)
+multi_future <- system.time(b <- run_future(20, project, tmpfolder(), conjunct = TRUE))
+b <- values(b)
+
+registerDoParallel(cl)
+doParallel <- system.time(c <- run_foreach(20, project, tmpfolder()))
+
+print(multi_future)
 print(sum(sapply(b, attr, which = "runtime")))
-# print(sapply(b, attr, which = "runtime"))
+print(sapply(b, attr, which = "runtime"))
+
 print(doParallel)
 print(sum(sapply(c, attr, which = "runtime")))
-#
+print(sapply(c, attr, which = "runtime"))
 
-unlink("./tests/testthat/data/mp", recursive = T, force = T)
+parallel::stopCluster(cl)
